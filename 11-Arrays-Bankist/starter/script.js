fcfeaf713a7c6251f6464c4c9740f14315af2820
Martin Leonardi <mtnleonardi@gmail.com>
2025-07-18 09:36:03 -0300

    Challenge 3 arrays completed

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -106,12 +106,31 @@ const test2 = [16, 6, 10, 5, 6, 1, 4]
 
 const humanAgesDogs = calcAverageHumanAge(test1)
 
-console.log(humanAgesDogs);
+// console.log(humanAgesDogs);
 
 // 2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
 const adultDogs = humanAgesDogs.filter((age) => {return age >= 18})
-console.log(adultDogs);
+// console.log(adultDogs);
 
 // 3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
 const avgAgeAdultDogs = adultDogs.reduce((acc, age) => {return acc + age}, 0) / adultDogs.length;
 console.log(avgAgeAdultDogs);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+const humanAgesDogsAvgChaining = test1.map((age) => {
+  return age <= 2 ? 2 * age : 16 + age * 4
+}).filter((age) => {
+  return age >= 18;
+}).reduce((acc, age) => {return acc + age}, 0) / adultDogs.length
+
+console.log(humanAgesDogsAvgChaining);
